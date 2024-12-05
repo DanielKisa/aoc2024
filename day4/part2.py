8910e97b853e30e918a02268919d709027b3be00
Daniel Kisa <daniel.kisa@thermofisher.com>
2024-12-05 08:54:50 +0100

    day4 part2

--- day4/part2.py
+++ day4/part2.py
@@ -2,27 +2,27 @@ import os
 
 from part1 import DIRECTIONS, verify_word, find_matches, read_data
 
+def find_x_mas_matches(mas_matches: list[dict]) -> list[dict]:
+    diagonal_mas_matches = [match for match in mas_matches if 0 not in match['direction']]
+    x_mas_matches = list()
+    for match_1 in diagonal_mas_matches:
+        match_1_center = (match_1['start'][0] + match_1['direction'][0],
+                          match_1['start'][1] + match_1['direction'][1])
+        for match_2 in diagonal_mas_matches:
+            if match_1 == match_2:
+                continue
+            
+            match_2_center = (match_2['start'][0] + match_2['direction'][0],
+                              match_2['start'][1] + match_2['direction'][1])
+            if match_1_center == match_2_center:
+                x_mas_matches.append(match_1)
+
+    return x_mas_matches
+
 if __name__ == '__main__':
     input_path = os.path.join('day4', 'input.txt')
     data = read_data(input_path)
     mas_matches = find_matches(data, 'MAS')
 
-    total_x_mas_count = 0
-    for match_1 in mas_matches:
-        if 0 in match_1['direction']:
-            continue
-        for match_2 in mas_matches:
-            if 0 in match_2['direction']:
-                continue
-
-            match_1_start_x, match_1_start_y = match_1['start']
-            match_2_start_x, match_2_start_y = match_2['start']
-            match_1_dir_x, match_1_dir_y = match_1['direction']
-            match_2_dir_x, match_2_dir_y = match_2['direction']
-            if (
-                (match_1_start_x + match_1_dir_x == match_2_start_x + match_2_dir_x) and
-                (match_1_start_y + match_1_dir_y == match_2_start_y + match_2_dir_y)
-                ):
-                total_x_mas_count += 1
-
-    print(f'{total_x_mas_count=}')
+    x_mas_matches = find_x_mas_matches(mas_matches)
+    print(f'{(len(x_mas_matches) // 2)=}')
